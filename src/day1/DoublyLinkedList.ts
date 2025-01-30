29ee553eac1385493e4c259c44b7b858ba1a6b99
zlucasftw <ilucasmcpvp@gmail.com>
2025-01-30 01:32:15 -0300

    add: Linked List - prepend, insertAt, & append

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,89 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>,
+    next?: Node<T>,
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     prepend(item: T): void {
+        const node = { value: item } as Node<T>;
+        
+        this.length++;
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+
     insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("oh no");
+        } 
+        
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        } else if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+        
+        this.length++;
+        let curr = this.head;
+        for (let i = 0; curr && i < idx; i++) {
+            curr = curr.next;
+        }
 
-}
+        curr = curr as Node<T>;
+        const node = { value: item } as Node<T>;
+
+        node.next = curr;
+        node.prev = curr.prev;
+        curr.prev = node;
+
+        if (curr.prev) {
+            curr.prev.next = curr;
+        }
+    }
+
     append(item: T): void {
+        
+        this.length++;
+        const node = {value : item} as Node<T>;
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
+        node.prev = this.tail;
+        this.tail.next = node;
+
+        this.tail = node;
+    }
+
     remove(item: T): T | undefined {
 
-}
+    }
+
     get(idx: number): T | undefined {
 
-}
+    }
+
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
